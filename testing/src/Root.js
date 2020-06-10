862df18d4eb8fb2ffdc14fbb9bf25a9ee045511f
Ibrahima D <ibrahima.diallo@display-interactive.com>
2020-06-10 15:18:24 +0200

    Add dev tool redux

--- testing/src/Root.js
+++ testing/src/Root.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import reduxPromise from 'redux-promise';
 import reducers from 'reducers';
 
 export default ({ children, initialState = {} }) => {
+
+  const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
+
   const store = createStore(
     reducers,
     initialState,
-    applyMiddleware(reduxPromise)
+    composeEnhancers(
+      applyMiddleware(reduxPromise)
+    )
   );
 
   return <Provider store={store}>{children}</Provider>;
